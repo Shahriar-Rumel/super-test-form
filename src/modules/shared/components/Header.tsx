00464cc619b1c66bed0082def354aa017666e454
Shahriar Rumel <rumelshahriarhere@gmail.com>
2023-10-20 12:07:47 +0600

    feat: add header for page

--- src/modules/shared/components/Header.tsx
+++ src/modules/shared/components/Header.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { HeaderContext } from '../../hooks/HeaderContext';
 
 const Header = () => {
-  return <div>Header</div>;
+  const { page } = useContext(HeaderContext);
+  return (
+    <header className="fixed left-0 right-0 top-[100px] flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-semibold text-violet-900">
+        Super Test Form
+      </h1>
+      <p className="text-xl text-slate-600 mt-4">{page}</p>
+    </header>
+  );
 };
 
 export default Header;
